30f825e687d4e35eafe6dca30011063b14d745c4
MarsiaRochelin <marsrochelin@pursuit.org>
2023-02-07 12:08:09 -0500

    controller initail build

---
+++ controllers/codersController.js
@@ -0,0 +1,18 @@
+// IMPORTS //
+const express = require("express");
+const coders = express.Router();
+
+// ROUTES //
+
+// INDEX
+
+// SHOW
+
+// CREATE
+
+// DELETE
+
+// UPDATE
+
+// EXPORTS //
+module.exports = coders;
