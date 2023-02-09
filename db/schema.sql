58f50ac6358f2ec5711ec6ae60359f0386d6e185
MarsiaRochelin <marsrochelin@pursuit.org>
2023-02-09 09:36:32 -0500

    seeded my table with1st project

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,15 @@ CREATE DATABASE projects_dev;
 
 \c projects_dev
 
+DROP TABLE IF EXISTS projects;
 CREATE TABLE projects (
     id SERIAL PRIMARY KEY, 
-    project_name VarChar(50), 
+    project_name VARCHAR(50), 
     project_description TEXT, 
+    technology_utilized TEXT, 
     start_date DATE, 
     due_date DATE, 
-    email VarChar(50)
+    email VARCHAR(50)
     );
+
 
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+\c projects_dev
+
+INSERT INTO projects (project_name, project_description, technology_utilized, start_date, due_date, email) 
+VALUES ('Tuner App', 'This app consited of building out both front and backend applications by using CRUD and MVP methods to have a functional full stack app. I currently need assistance tweaking the logic post merging routes in the backend and setting up CSS for my frontend app to ensure a bettter UX and an appealing UI', 'Postgres, Express, React, Node, CSS, and Git', '2023-02-25', '2023-03-05', 'marsrochelin@pursuit.org');
