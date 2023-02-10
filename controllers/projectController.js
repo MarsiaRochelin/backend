730fa41c5c9d58b178024adddeead884ae75b2b1
MarsiaRochelin <marsrochelin@pursuit.org>
2023-02-10 13:15:39 -0500

    working on my crUd fuctionality

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -6,6 +6,7 @@ const {
   getSingleProject,
   newProject,
   deleteProject,
+  updateProject,
 } = require("../queries/projects");
 const { checkProjectName } = require("../validations/checkProjects");
 
@@ -54,6 +55,15 @@ project.delete("/:id", async (req, res) => {
 });
 
 // UPDATE
+project.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedProject = await updateProject(id, req.body);
+    res.status(200).json(updatedProject);
+  } catch (error) {
+    res.status(404).json({ error: "project not found" });
+  }
+});
 
 // EXPORTS //
 module.exports = project;
--- queries/projects.js
+++ queries/projects.js
@@ -1,5 +1,7 @@
+// CONFIGURATION //
 const db = require("../db/dbConfig");
 
+//INDEX
 const getAllProjects = async () => {
   try {
     const allProjects = await db.any("SELECT * FROM projects");
@@ -9,6 +11,7 @@ const getAllProjects = async () => {
   }
 };
 
+//SHOW
 const getSingleProject = async (id) => {
   try {
     const singleProject = await db.one(
@@ -21,6 +24,7 @@ const getSingleProject = async (id) => {
   }
 };
 
+//CREATE
 const newProject = async (project) => {
   try {
     const newProject = await db.one(
@@ -40,6 +44,7 @@ const newProject = async (project) => {
   }
 };
 
+//DELETE
 const deleteProject = async (id) => {
   try {
     const deletedProject = await db.one(
@@ -52,9 +57,32 @@ const deleteProject = async (id) => {
   }
 };
 
+//UPDATE
+const updateProject = async (id) => {
+  try {
+    const updatedProject = await db.one(
+      "UPDATE projects SET project_name=$1, project_description=$2, technology_utilized=$3, start_date=$4, due_date=$5, email=$6 Returning *",
+      [
+        project.project_name,
+        project.project_description,
+        project.technology_utilized,
+        project.start_date,
+        project.due_date,
+        project.email,
+        id,
+      ]
+    );
+    return updatedProject;
+  } catch (error) {
+    return error;
+  }
+};
+
+// EXPORT //
 module.exports = {
   getAllProjects,
   getSingleProject,
   newProject,
   deleteProject,
+  updateProject,
 };
